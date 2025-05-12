7833e6aa0a860aec6bab452d1a42dade7fd050dc
Amrit <amritfordevops@gmail.com>
2025-05-12 21:52:14 +0530

    Change reverted

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 var express = require('express')
-const client = require('prom-client');
-
-
 var app = express()
-const collectDefaultMetrics = client.collectDefaultMetrics;
-collectDefaultMetrics();
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
@@ -16,13 +11,3 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
-
-
-app.get('/metrics', async (req, res) => {
-  res.set('Content-Type', client.register.contentType);
-  res.end(await client.register.metrics());
-});
-
-app.listen(5000, () => {
-  console.log('Server running on port 3000');
-});
